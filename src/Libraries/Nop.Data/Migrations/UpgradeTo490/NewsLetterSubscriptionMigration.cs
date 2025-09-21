8270a2ab9d573184bcd56389d59a095b9daff7ea
derrickmadden1 <57037531+derrickmadden1@users.noreply.github.com>
2025-09-21 19:25:31 +0100

    Upstream merge (#5)
    * #7852 GetCustomerRoleBySystemNameAsync in PermissoinService and CustomerService
    
    * #7850 The NullView class is in the wrong namespace
    
    * #7849 Make a base class for Result classes such as VoidPaymentResult
    
    * #7858 Simplify the definition of empty classes
    
    * #196 Update LigerShark.WebOptimizer.Core package version to 3.0.477
    
    * #7390 Fixed issue with field toggling on the Edit menu item details page
    
    * #7862 Admin area. There is no way to make the base menu item invisible
    
    * #7739 #5986 Fixed on "Allow to delete image thumbs"
    
    * #5720 New changes on "Added the partial modifier to record and class"
    
    * #7732 Automated SEO optimization with AI tools
    
    Fixed checking for the presence of required fields for the standard language block
    
    * Added links for documentation
    
    * #7801 Add column NewsLetterSubscription fails due to missing SetExistingRowsTo()
    
    * NewsLetterSubscriptionMigration add using
    
    ---------
    
    Co-authored-by: Sergey Koshelev <sergey.k@nopcommerce.com>
    Co-authored-by: Alexey Anokhin <exile.developer@gmail.com>
    Co-authored-by: DmitriyKulagin <kulagin87@gmail.com>

--- src/Libraries/Nop.Data/Migrations/UpgradeTo490/NewsLetterSubscriptionMigration.cs
+++ src/Libraries/Nop.Data/Migrations/UpgradeTo490/NewsLetterSubscriptionMigration.cs
@@ -1,7 +1,7 @@
-﻿using System.Data;
-using FluentMigrator;
+﻿using FluentMigrator;
 using Nop.Core.Domain.Messages;
 using Nop.Data.Extensions;
+using System.Data;
 
 namespace Nop.Data.Migrations.UpgradeTo490;
 
