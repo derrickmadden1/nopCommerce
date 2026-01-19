19e0f4e26e6b6ef32a48b0206f7e263dcb256b16
derrickmadden1 <57037531+derrickmadden1@users.noreply.github.com>
2026-01-19 09:06:43 +0000

    Add and update square plugin. This is used on live site. Intend to replace with alternative once upgraded

---
+++ src/Plugins/Nop.Plugin.Payments.Square/Domain/TransactionMode.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Nop.Plugin.Payments.Square.Domain;
+
+/// <summary>
+/// Represents payment transaction mode enumeration
+/// </summary>
+public enum TransactionMode
+{
+    /// <summary>
+    /// Authorize
+    /// </summary>
+    Authorize = 0,
+
+    /// <summary>
+    /// Charge (authorize and capture)
+    /// </summary>
+    Charge = 2
+}
