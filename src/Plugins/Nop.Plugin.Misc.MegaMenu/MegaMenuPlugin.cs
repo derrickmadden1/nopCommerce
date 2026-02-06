89556ca7187e8547ff70ac64b62ee6f53e7e35c6
derrickmadden1 <57037531+derrickmadden1@users.noreply.github.com>
2026-02-06 19:20:55 +0000

    Add TheSolidInspired theme and MegaMenu plugin

---
+++ src/Plugins/Nop.Plugin.Misc.MegaMenu/MegaMenuPlugin.cs
@@ -0,0 +1,21 @@
+﻿using Nop.Services.Common;
+using Nop.Services.Plugins;
+
+namespace Nop.Plugin.Misc.MegaMenu
+{
+    public class MegaMenuPlugin : BasePlugin, IMiscPlugin
+    {
+        public override string GetConfigurationPageUrl()
+            => string.Empty; // no config page
+
+        public override async Task InstallAsync()
+        {
+            await base.InstallAsync();
+        }
+
+        public override async Task UninstallAsync()
+        {
+            await base.UninstallAsync();
+        }
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Misc.MegaMenu/Models/MegaMenuItemModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Nop.Plugin.Misc.MegaMenu.Models
+{
+    public class MegaMenuItemModel
+    {
+        public string Name { get; set; }
+        public string SeName { get; set; }
+        public IList<MegaMenuItemModel> Children { get; set; } = new List<MegaMenuItemModel>();
+    }
+}
