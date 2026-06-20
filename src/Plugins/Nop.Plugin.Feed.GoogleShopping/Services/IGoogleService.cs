529bc9cb1c1de794c2e2b2575bd904d306a91351
derrickmadden1 <57037531+derrickmadden1@users.noreply.github.com>
2026-06-20 20:42:33 +0100

    Add Google Shopping plugin and upgraded to .net10 (#104)
    * Add Google Shopping plugin and upgraded to .net10

---
+++ src/Plugins/Nop.Plugin.Feed.GoogleShopping/Services/IGoogleService.cs
@@ -0,0 +1,20 @@
+﻿using Nop.Plugin.Feed.GoogleShopping.Domain;
+
+namespace Nop.Plugin.Feed.GoogleShopping.Services;
+
+public partial interface IGoogleService
+{
+    Task DeleteGoogleProductAsync(GoogleProductRecord googleProductRecord);
+
+    Task<IList<GoogleProductRecord>> GetAllAsync();
+
+    Task<GoogleProductRecord> GetByIdAsync(int googleProductRecordId);
+
+    Task<GoogleProductRecord> GetByProductIdAsync(int productId);
+
+    Task InsertGoogleProductRecordAsync(GoogleProductRecord googleProductRecord);
+
+    Task UpdateGoogleProductRecordAsync(GoogleProductRecord googleProductRecord);
+
+    Task<IList<string>> GetTaxonomyListAsync();
+}
