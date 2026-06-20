383d850101c5358289833345f6ccc9dcfd5762f3
derrickmadden1 <57037531+derrickmadden1@users.noreply.github.com>
2026-06-20 20:33:07 +0100

    Add Google Shopping plugin and upgraded to .net10

---
+++ src/Plugins/Nop.Plugin.Feed.GoogleShopping/Data/SchemaMigration.cs
@@ -0,0 +1,22 @@
+using FluentMigrator;
+using Nop.Data.Extensions;
+using Nop.Data.Migrations;
+using Nop.Plugin.Feed.GoogleShopping.Domain;
+
+namespace Nop.Plugin.Feed.GoogleShopping.Data;
+
+[NopMigration("2020/06/02 12:00:00", "Feed.GoogleShopping base schema", MigrationProcessType.Installation)]
+public class SchemaMigration : AutoReversingMigration
+{
+    #region Methods
+
+    /// <summary>
+    /// Collect the UP migration expressions
+    /// </summary>
+    public override void Up()
+    {
+        this.CreateTableIfNotExists<GoogleProductRecord>();
+    }
+
+    #endregion
+}
---
+++ src/Plugins/Nop.Plugin.Feed.GoogleShopping/Models/GoogleProductSearchModel.cs
@@ -0,0 +1,10 @@
+﻿using Nop.Web.Framework.Models;
+
+namespace Nop.Plugin.Feed.GoogleShopping.Models;
+
+/// <summary>
+/// Represents Google product list search model
+/// </summary>
+public partial record GoogleProductSearchModel : BaseSearchModel
+{
+}
